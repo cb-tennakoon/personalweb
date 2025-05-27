06305e69425f583162644e0fdfdae2c136fd62c1
Chathuranga <cbthraeka1995@gmail.com>
2025-05-27 09:59:53 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () =>{
+    return <div className="min-h-screen ">
+        <ThemeToggle/>
+        {/* {Theme Toggle} */}
+        {/* (Background Effects) */}
+        {/*Navbar */}
+        {/* Main Content */}
+        {/* Footer */}
+    </div>
+};
+
