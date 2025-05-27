d2a6e604f9fcb10c63b723f6f6cc1d91fe7870fb
Chathuranga <cbthraeka1995@gmail.com>
2025-05-27 10:22:34 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route  index element={<Home/>}></Route >
+          <Route  index element={<Home/>}/>
           <Route  path="*" element={<NotFound/>}/>
         </Routes> 
       </BrowserRouter>
